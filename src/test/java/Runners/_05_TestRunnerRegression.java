bb74fc8a327b476ddae69856bfc3b9d8b2700b6d
YaseminGurnal <ysmngurnal@gmail.com>
2025-10-29 16:32:57 +0300

    Test Runner Regression

--- src/test/java/Runners/_05_TestRunnerRegression.java
+++ src/test/java/Runners/_05_TestRunnerRegression.java
@@ -4,10 +4,9 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        tags = "@SmokeTest",//Sadece tag i belirtilen senaryoları çalıştırır.
+        tags = "RegressionTest or @SmokeTest",//Sadece tag i belirtilen senaryoları çalıştırır.
         features = {"src/test/java/FeatureFiles"},
         glue = {"StepDefinitions"}
 )
-
-public class _04_TestRunnerSmoke extends AbstractTestNGCucumberTests {
+public class _05_TestRunnerRegression extends AbstractTestNGCucumberTests {
 }
