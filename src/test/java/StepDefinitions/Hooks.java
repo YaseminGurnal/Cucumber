9d024bfe72424e0919813d19ef2728c5eeffddfb
YaseminGurnal <ysmngurnal@gmail.com>
2025-10-30 12:51:40 +0300

    Extend Report

--- src/test/java/StepDefinitions/Hooks.java
+++ src/test/java/StepDefinitions/Hooks.java
@@ -2,11 +2,19 @@ package StepDefinitions;
 
 import Utilities.GWD;
 import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks {
     //Cucumber a ait her senaryadon sonra çalışan annotation
     @After
-    public void After(){
+    public void After(Scenario senaryo){ //Cucumber ın otomatik senaryo ile ilgili bilgiler değişkeni
+        if (senaryo.isFailed()){
+            TakesScreenshot ts=((TakesScreenshot) GWD.getDriver());
+            byte[] hafizadakiHali=ts.getScreenshotAs(OutputType.BYTES);
+            senaryo.attach(hafizadakiHali, "image/png", "screenshot name");
+        }
         GWD.quitDriver();
     }
 }
